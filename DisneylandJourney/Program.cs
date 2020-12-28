4d9abdf9a9f1c3681731839f0c4784759abebdd7
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2020-12-28 17:49:35 +0100

    3 problems solved

---
+++ DisneylandJourney/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01.DisneylandJourney
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double journeyCost = double.Parse(Console.ReadLine());
+            int input = int.Parse(Console.ReadLine());
+            int[] months = new int[input];
+
+            double moneySaved = 2 * journeyCost * 0.25;
+            double moneyPerMonth = journeyCost * 0.25;
+            for (int i = 2; i < months.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    moneySaved = moneySaved * 0.84;
+                    moneySaved += moneyPerMonth;
+                }
+                else
+                {
+                    moneySaved += moneyPerMonth;
+                }
+
+                if (i == 3 || i ==7 || i == 11)
+                {
+                    moneySaved = (moneySaved-moneyPerMonth) * 1.25;
+                    moneySaved += moneyPerMonth;
+                }
+            }
+            if (moneySaved > journeyCost)
+            {
+                Console.WriteLine($"Bravo! You can go to Disneyland and you will have {(moneySaved-journeyCost):F2}lv. for souvenirs.");
+            }         
+            else
+            {
+                Console.WriteLine($"Sorry. You need {(journeyCost-moneySaved):F2}lv. more.");
+            }
+        }
+    }
+}
---
+++ SchoolLibrary/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SchoolLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> books = Console.ReadLine()
+                                        .Split("&", StringSplitOptions.RemoveEmptyEntries)
+                                        .ToList();
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "Done")
+            {
+                string[] cmdArgs = input.Split(" | ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string command = cmdArgs[0];
+                string bookName = cmdArgs[1];
+                string bookNameToSwap = string.Empty;
+
+                switch (command)
+                {
+                    case "Add Book":
+
+                        if (!books.Contains(bookName))
+                        {
+                            books.Insert(0, bookName);
+                        }
+                        break;
+                    case "Take Book":
+                        if (books.Contains(bookName))
+                        {
+                            books.Remove(bookName);
+                        }
+                        break;
+                    case "Swap Books":
+                        bookNameToSwap = cmdArgs[2];
+                        if (books.Contains(bookName) && books.Contains(bookNameToSwap))
+                        {
+                            int firstIndex = books.FindIndex(x => x == bookName);
+                            int secondIndex = books.FindIndex(x => x == bookNameToSwap);
+
+    
+                            string temporar = books[firstIndex];
+                            books[firstIndex] = bookNameToSwap;
+                            books[secondIndex] = bookName;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                        break;
+                    case "Insert Book":
+                        books.Add(bookName);
+                        break;
+
+                    case "Check Book":
+                        int index = int.Parse(cmdArgs[1]);
+                        if (index < 0 || index > books.Count - 1)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{books[index]}");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(", ", books));
+        }
+    }
+}
